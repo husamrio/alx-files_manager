4c2c3bef1131cd9af949c9ca5b8d74edae4c3e03
husamrio <zulfiqrd@gmail.com>
2024-03-09 19:40:34 +0300

    update

---
+++ libs/middlewares.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+/**
+ * Adds middlewares to the given express application.
+ * **************************************************
+ * --------------------------------------------------
+ * @param {express.Express} api The express application.
+ */
+const injectMiddlewares = (api) => {
+  api.use(express.json({ limit: '200mb' }));
+};
+
+export default injectMiddlewares;
